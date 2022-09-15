e7d2faa35a75ec1aff488781b5aec6f84d41bab0
Yuriy <yurchest@gmail.com>
2022-09-16 02:24:39 +0300

    init (^_^)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import {queries} from "@testing-library/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [posts, setPost] = useState([
+        {id: 1, title: "Javascript", body: 'Description'},
+        {id: 2, title: "Javascript 2", body: 'Description'},
+        {id: 3, title: "Javascript 3", body: 'Description'},
+    ])
+    const [title, setTitle] = useState('dsf')
+    const addNewPost = (e) => {
+        e.preventDefault()
+        console.log(title)
+    }
+
+
+    return (
+        <div className="App">
+            <form>
+                <MyInput
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    type="text"
+                    placeholder="Название поста"
+                />
+                <MyInput type="text" placeholder="Описание поста"/>
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
+            </form>
+            <PostList posts={posts} title="Посты про JS"/>
+
+        </div>
+    );
 }
 
 export default App;
