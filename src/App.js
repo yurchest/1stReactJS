d103cb0c4f50dfeef2f6ea58808c684a4551173d
Yuriy <yurchest@gmail.com>
2022-09-16 12:21:15 +0300

    commit :)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
@@ -7,34 +7,73 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import {queries} from "@testing-library/react";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
 
-    const [posts, setPost] = useState([
-        {id: 1, title: "Javascript", body: 'Description'},
-        {id: 2, title: "Javascript 2", body: 'Description'},
-        {id: 3, title: "Javascript 3", body: 'Description'},
+    const [posts, setPosts] = useState([
+        {id: 1, title: "aa", body: 'ddd'},
+        {id: 2, title: "bb 2", body: 'ss'},
+        {id: 3, title: "cc 3", body: 'gg'},
     ])
-    const [title, setTitle] = useState('dsf')
-    const addNewPost = (e) => {
-        e.preventDefault()
-        console.log(title)
+
+    const [selectedSort, setSelectedSort] = useState('')
+    const [searchQuery, setSearchQuery] = useState('')
+
+    function getSortedPosts() {
+        console.log('Sorted')
+        if(selectedSort) {
+            return [...posts.sort((a , b) => a[selectedSort].localeCompare(b[selectedSort]))]
+        }
+        return posts;
+    }
+
+    const sortedPosts = getSortedPosts()
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
     }
 
 
     return (
         <div className="App">
-            <form>
+            <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0'}}/>
+            <div>
                 <MyInput
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
-                    type="text"
-                    placeholder="Название поста"
+                    value={searchQuery}
+                    onChange={e => setSearchQuery(e.target.value)}
+                    placeholder="Search..."
                 />
-                <MyInput type="text" placeholder="Описание поста"/>
-                <MyButton onClick={addNewPost}>Создать пост</MyButton>
-            </form>
-            <PostList posts={posts} title="Посты про JS"/>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="Sorting"
+                    option={[
+                        {value: 'title', name: "by name"},
+                        {value: 'body', name: "by body"},
+                    ]}
+                />
+                
+            </div>
+            
+            {posts.length
+                ? <PostList remove={removePost} posts={sortedPosts} title="Посты про JS"/>
+                :
+                <h1 style={{textAlign: 'center'}}>
+                    No Posts
+                </h1>
+            }
+
 
         </div>
     );
